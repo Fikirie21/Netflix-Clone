1bf2285430343ec56719a7bbcf5599bd18bc4313
FikirMersha <162644747+FikirMersha@users.noreply.github.com>
2024-06-01 00:29:06 -0400

    netflix

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from "../../Component/Header/Header";
+import Footer from "../../Component/Footer/Footer";
+import Banner from "../../Component/Banner/Banner";
+import RowList from "../../Component/Rows/RowList/RowList"
+function Home() {
+  return (
+    <>
+        <Header/>
+        <Banner/>
+        <RowList/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Home
